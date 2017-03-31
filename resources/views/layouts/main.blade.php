667e3ddb7cb9b435817b8643c1925263f088e0f8
ferdinandfrank <ferdinand-frank@web.de>
2017-03-31 19:56:41 +0200

    Fixed footer copyright

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,7 +35,7 @@
         <div class="container">
             <div class="content has-text-centered">
                 <p>
-                    &copy; {{ \Carbon\Carbon::now()->year }} <strong>Starmee</strong> by <a href="http://spoferan.com">Spoferan</a>.
+                    &copy; {{ \Carbon\Carbon::now()->year }} <a href="http://spoferan.com">Spoferan</a>.
                 </p>
                 <p>
                     <a class="icon" href="https://www.facebook.com/spoferan">
